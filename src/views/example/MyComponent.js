5d7616724c23ad12d6186f4367c7037b751d39ce
hoangthiep92 <hoangduy-thiep@sunshou.co.jp>
2023-12-05 11:17:39 +0900

    hoc xong clip 9

--- src/views/example/MyComponent.js
+++ src/views/example/MyComponent.js
@@ -2,10 +2,79 @@ import React from "react";
 
 class MyComponent extends React.Component {
 
+// bien state key:value
+state = {
+    name: 'Thiep',
+    channel: 'Hoi dan IT'
+}
+
+
+/*
+JSX return block
+ */
+
+// vi du co ban khi su dung DOM
+handleClickButton = () => {
+    alert('click me')
+}
+handleOnChangeName = (event) => {
+    //kiem tra cac gia trij event
+    // console.log(event.target.value, 
+    //     'event target: ',
+    //     event.target,
+    //     'event object: ',
+    //     event)
+        this.setState({
+            name: event.target.value
+        })
+}
+
     render() {
+
         return (
-            <div>hello my component </div>
+            // <div className="abc">
+            //     <div className="firstClass">
+            //     {console.log('My name is:', name)}
+            // hello my component, My name is {name}
+            //     </div>
+
+            //     <div className="secondCalss">
+            //         block nho ban trong
+            //     </div>
+            // </div>
+            // <React.Fragment> // khi muon return nhieu block
+            //     <div className="firstClass">
+            //         {console.log('My name is:', name)}
+            //         hello my component, My name is {name}
+            //     </div>
+
+            //     <div className="secondCalss">
+            //         block nho ban trong
+            //     </div>
+            // </React.Fragment>
+
+            // cach 2 khi muon dung Fragment
+            <> 
+            <div className="firstClass">
+                    {console.log('My name is:', this.state.name)}
+                    <input onChange={(event) => this.handleOnChangeName(event)} 
+                    type="text" 
+                    value={this.state.name}/>
+                    hello my component, My name is {this.state.name}
+                </div>
+
+                <div className="secondCalss">
+                    {/* block nho ban trong */}
+                    My channel: {this.state.channel}
+                </div>
+
+                <div className="third">
+                    <button onClick={() => this.handleClickButton()}>click me</button>
+                </div>
+
+            </>
         )
+
     }
 }
 
